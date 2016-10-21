7ed9287c5e3f7f1d97506aa3eb10a37e03eb4ede
Shuyi Wang <sdjksdafji@gmail.com>
2016-10-21 16:14:32 -0700

    add arg parse

--- close_cluster_when_job_done.py
+++ close_cluster_when_job_done.py
@@ -3,22 +3,42 @@ import subprocess
 from time import sleep
 import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument("job_id", help="Job ID from Google Cloud Dataproc Jobs")
+parser.add_argument("--cluster_name", default="friend-graph-dataproc",
+                    help="Cluster Name from Google Cloud Dataproc Jobs")
+parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+args = parser.parse_args()
+
+jobId = args.job_id
+clusterName = args.cluster_name
+
+print("Trying to monitor job with ID: " + jobId)
+print("On cluster with name: " + clusterName)
+
 jobTerminated = False
-jobId = "76f83e8f-4ea5-49da-a2b3-fdf428d90055"
-clusterName = "friend-graph-dataproc"
+counter = 0
 
 while not jobTerminated:
-  out = subprocess.getoutput("gcloud dataproc jobs describe " + jobId)
-  for rawMsg in out.split("\n"):
-    msg = rawMsg.strip()
-    #print(msg)
-    if "state:" in msg and (not "state: PENDING" in msg) and (not "state: RUNNING" in msg) and (not "state: SETUP_DONE" in msg):
-      print(msg)
-      jobTerminated = True
-      break;
-  print("Waiting for job to finish ...")
-  sys.stdout.flush()
-  sleep(5)
+    out = subprocess.getoutput("gcloud dataproc jobs describe " + jobId)
+    if args.verbose:
+        print(out)
+    for rawMsg in out.split("\n"):
+        msg = rawMsg.strip()
+
+        if "state:" in msg and (not "state: PENDING" in msg) and (not "state: RUNNING" in msg) and (
+                not "state: SETUP_DONE" in msg):
+            print(msg)
+            jobTerminated = True
+            break;
+
+    if not jobTerminated:
+        print("\r" + "Waiting for job to finish " + ("." * (counter + 1)) + "   ", end="\r")
+        sys.stdout.flush()
+        sleep(5)
+    counter += 1
+    counter %= 3
+
 print("Job finished. Ready to delete cluster. Waiting 5 min inorder to wait for eventual consistency")
 sleep(5 * 60)
 deleteClusterOutput = subprocess.getoutput("echo Y | gcloud dataproc clusters delete " + clusterName)
